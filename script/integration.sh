1124bd67bdd291ea2989cf39e07f778bf310c236
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-02-13 21:23:09 +0100

    ci: run benchmarks with hyperfine
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- script/integration.sh
+++ script/integration.sh
@@ -2,10 +2,7 @@
 
 set -xe
 
-curl $1/status
+cmd_status="curl $1/status"
+cmd_create_json="curl -X POST -v -H \"Content-Type: application/json\"  --data \"{\\\"from\\\":\\\"alice@example.com\\\",\\\"to\\\":\\\"bob@example.com\\\",\\\"params\\\":{}}\" $1/templates/user-login"
 
-curl -X POST -v \
-  -H "Content-Type: application/json" \
-  --data "{\"from\":\"alice@example.com\",\"to\":\"bob@example.com\",\"params\":{}}" \
-  $1/templates/user-login
-
+~/.cargo/bin/hyperfine --export-json hyperfine.json "$cmd_status" "$cmd_create_json"
