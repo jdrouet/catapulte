6ebef8c43e884e4f76679a07254053c1218879be
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-05-18 07:49:00 +0200

    enhancement(server): status endpoint now returns uptime
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- src/controller/status.rs
+++ src/controller/status.rs
@@ -1,8 +1,16 @@
 use actix_web::{get, HttpResponse};
+use serde_json::json;
+use std::time::Instant;
+
+lazy_static! {
+    static ref STARTUP: Instant = Instant::now();
+}
 
 #[get("/status")]
 pub async fn handler() -> HttpResponse {
-    HttpResponse::NoContent().finish()
+    HttpResponse::Ok().json(json!({
+        "uptime": STARTUP.elapsed().as_secs(),
+    }))
 }
 
 // LCOV_EXCL_START
@@ -17,7 +25,7 @@ mod tests {
     async fn status_success() {
         let req = test::TestRequest::get().uri("/status").to_request();
         let res = execute_request(req).await;
-        assert_eq!(res.status(), StatusCode::NO_CONTENT);
+        assert_eq!(res.status(), StatusCode::OK);
     }
 }
 // LCOV_EXCL_END
