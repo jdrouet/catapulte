e2a0fb3463e64cc4c87bbd0237aa7aeb73306074
Jérémie Drouet <jeremie.drouet@gmail.com>
2024-03-09 21:54:45 +0100

    feat: allow to specify config path through env variable
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- src/action/serve.rs
+++ src/action/serve.rs
@@ -47,7 +47,12 @@ impl Configuration {
 #[derive(clap::Parser)]
 pub(crate) struct Action {
     /// Path to the configuration toml file, default to /etc/catapulte/catapulte.toml.
-    #[clap(short, long, default_value = "/etc/catapulte/catapulte.toml")]
+    #[clap(
+        short,
+        long,
+        default_value = "/etc/catapulte/catapulte.toml",
+        env = "CATAPULTE_CONFIG"
+    )]
     pub config_path: String,
 }
 
